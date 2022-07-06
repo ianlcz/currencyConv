9f4e4de4634e1e8a3afbdbd4aefed20a514aec2b
Yann LE COZ <yann.lecoz@ynov.com>
2022-07-06 19:10:23 +0200

    Set command-line interface

--- app.ts
+++ app.ts
@@ -1,15 +1,41 @@
+import { Command, Option } from "commander";
+
 import { Converter } from "./models/Converter.model";
 import { Currency } from "./models/Currency.model";
 
 const main = async () => {
+  const app = new Command();
+  app.version(
+    "v1.0.0",
+    "-v, --version",
+    "output the current version",
+  );
+  app.requiredOption("-a, --amount <value>", "amount to convert");
+  app
+    .addOption(
+      new Option(
+        "-s, --source [currency]",
+        "currency corresponding to the amount",
+      ).default("EUR"),
+    )
+    .addOption(
+      new Option(
+        "-t, --target [currency]",
+        "currency in which you want to convert the amount",
+      ).default("USD"),
+    );
+  app.parse(process.argv);
+
+  const { amount, source, target } = app.opts();
+
   // Instantiation of Currency classes (source and target)
-  const currencySource = new Currency("BTC");
-  const currencyTarget = new Currency("USD");
+  const currencySource = new Currency(source);
+  const currencyTarget = new Currency(target);
   // Get exchange rate for the target currency
   await currencyTarget.getExchangeRate(currencySource.name);
 
   // Instantiation of Converter class
-  const converter = new Converter(20, currencySource, currencyTarget);
+  const converter = new Converter(amount, currencySource, currencyTarget);
   converter.setConversion();
 
   // Display result of conversion
